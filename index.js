bf0f6a2fa53fd19312dc5f19827a198788b0cf6d
shreyas <shreyasgirjapure1122@gmail.com>
2022-12-28 23:58:15 +0530

    adding first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+const url =
+  "https://d2w00000kn3veeat-dev-ed.develop.my.salesforce.com/services/apexrest/CardDetails";
+const options = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization:
+      "Bearer 00D2w00000KN3vE!AR0AQFlR9J7tGe2J2sTcLA6s.vsHGBcQ2pM9UELiPr3pfsmiS.ehDjd0z0Nviyf8gTNkYCoeIegqvgNc74emwOVQw_8V5iV.",
+  },
+};
+
+app.get("/cardDetails", (req, res) => {
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => res.send(json))
+    .catch((err) => console.error("error:" + err));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
